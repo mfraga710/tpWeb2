0ffe1fcf7d6d0d32bb48b4c6fed3495ea5acb70e
ladyshanai <paulalezcano21@gmail.com>
2021-05-25 15:49:55 -0300

    se agregan prod al array, descrip Y se modif json

--- datos/productos.php
+++ datos/productos.php
@@ -28,79 +28,192 @@ $productos = array (
     ),
     '2'=> array(
         'id'=> 2,
-        'nombre'=>'Pantalones',
+        'nombre'=>'Fujifilm Instax Mini 9',
         'marca'=> 2,
-        'precio'=> 856,
-        'descripcion'=> 'Pantalones rotos',
+        'precio'=> 10500,
+        'descripcion'=> 'Cámara instantánea,
+                        Distancia focal del lente: 60 mm
+                        Tipo de cámara: Instantánea
+                        Tamaño de la imagen: 62 mm x 46 mm
+                        Apertura máxima del lente: f/12.7
+                        Compensaciones de la exposición: Manual
+                        Sensibilidad ISO: ISO 100-1600
+                        Con flash incorporado: Sí
+                        Con espejo para selfies: Sí
+                        Tipo de pila: AA
+                        Ancho x Profundidad x Altura: 118.3 mm x 68.2 mm x 116 mm
+                        Peso: 307 g',
         'categoria'=> 1,
-        'imagenL'=> '1.jpg',
-        'imagenS1'=> '1s1.jpg',
-        'imagenS2'=> '1.jpg',
-        'imagenS3'=> '1.jpg',
+        'imagenL'=> 'fuji_azul_160x160.jpg',
+        'imagenS1'=> 'fuji_azul_500x375.jpg',
+        'imagenS2'=> 'fuji_azul_160x160.jpg',
+        'imagenS3'=> 'fuji_azul_160x160.jpg',
         'comentarios'=> 1,
         'activa'=> TRUE
 
     ),
     '3'=> array(
         'id'=> 3,
-        'nombre'=>'Buzos',
+        'nombre'=>'Tarjeta de memoria',
         'marca'=> 3,
         'precio'=> 628,
-        'descripcion'=> 'No abrigan nada',
-        'categoria'=> 1,
-        'imagenL'=> '1.jpg',
-        'imagenS1'=> '1s1.jpg',
-        'imagenS2'=> '1.jpg',
-        'imagenS3'=> '1.jpg',
+        'descripcion'=> 'Capacidad: 32 GB
+                         Formato de la tarjeta: MicroSDHC
+                         Resolución máxima de vídeo: Full HD
+                        Velocidad de lectura: 80 MB/s
+                        Incluye adaptador SD: Sí',
+        'categoria'=> 3,    
+        'imagenL'=> '10.jpg',
+        'imagenS1'=> '10.jpg',
+        'imagenS2'=> '10.jpg',
+        'imagenS3'=> '10.jpg',
         'comentarios'=> 1,
         'activa'=> TRUE
 
     ),
     '4'=> array(
         'id'=> 4,
-        'nombre'=>'Calzones',
-        'marca'=> 4,
+        'nombre'=>'Cámara compacta',
+        'marca'=> 1,
         'precio'=> 8000,
-        'descripcion'=> 'Calzones usados',
+        'descripcion'=> 'Resolución de 16 Mpx f/3-6.5.
+                         Resolución de 16 Mpx.
+                         El obturador tiene una velocidad máxima de: 1/1500 s.
+                         Pantalla de 3 ".
+                         20 MB de memoria interna.
+                         Al tener enfoque manual esta cámara captura más detalles en las fotografías.
+                         Con modo autofoco.
+                         Lente incluido: Nikkor 4.0-160 mm f/3-6.5 VR.
+                         Zoom digital de 4x.
+                         Se conecta por: Wi-Fi, Bluetooth.
+                         Calidad sin límites en tus fotografías.',
         'categoria'=> 1,
-        'imagenL'=> '1.jpg',
-        'imagenS1'=> '1s1.jpg',
-        'imagenS2'=> '1.jpg',
-        'imagenS3'=> '1.jpg',
+        'imagenL'=> '14.jpg',
+        'imagenS1'=> '15.jpg',
+        'imagenS2'=> '15.jpg',
+        'imagenS3'=> '15.jpg',
         'comentarios'=> 1,
         'activa'=> TRUE
 
     ),    
     '5'=> array(
         'id'=> 5,
-        'nombre'=>'Zapatillas',
-        'marca'=> 5,
+        'nombre'=>'Cámara sin espejo',
+        'marca'=> 2,
         'precio'=> 7000,
-        'descripcion'=> 'Zapatillas con aeroventilas',        
+        'descripcion'=> 'Tipo de cámara: Sin espejo
+                         Tamaño de la imagen: 62 mm x 99 mm
+                         Apertura máxima del lente: f/14
+                         Sensibilidad ISO: ISO 800
+                         Con flash incorporado: Sí
+                         Con espejo para selfies: Sí
+                          Tipo de pila: AA
+                          Ancho x Profundidad x Altura: 167.8 mm x 94.7 mm x 120.9 mm
+                          Peso: 612 g',        
         'categoria'=> 1,
-        'imagenL'=> '1.jpg',
-        'imagenS1'=> '1s1.jpg',
-        'imagenS2'=> '1.jpg',
-        'imagenS3'=> '1.jpg',
+        'imagenL'=> '16.jpg',
+        'imagenS1'=> '17.jpg',
+        'imagenS2'=> '17.jpg',
+        'imagenS3'=> '17.jpg',
         'comentarios'=> 1,
         'activa'=> TRUE
 
     ),    
     '6'=> array(
         'id'=> 6,
-        'nombre'=>'Medias',
-        'marca'=> 5,
+        'nombre'=>'Tarjeta de memoria 16g',
+        'marca'=> 3,
         'precio'=> 300,
-        'descripcion'=> 'Medias sucias',        
-        'categoria'=> 1,
-        'imagenL'=> '1.jpg',
-        'imagenS1'=> '1s1.jpg',
-        'imagenS2'=> '1.jpg',
-        'imagenS3'=> '1.jpg',
+        'descripcion'=> 'Capacidad: 32 GB
+                        Formato de la tarjeta: MicroSDHC
+                        Resolución máxima de vídeo: Full HD
+                        Velocidad de lectura: 80 MB/s
+                        Incluye adaptador SD: Sí',        
+        'categoria'=> 3,
+        'imagenL'=> '8.jpg',
+        'imagenS1'=> '8.jpg',
+        'imagenS2'=> '8.jpg',
+        'imagenS3'=> '8.jpg',
         'comentarios'=> 1,
         'activa'=> TRUE
 
-    ),    
+    ),
+    '7'=> array(
+        'id'=> 7,
+        'nombre'=>'Camara digital',
+        'marca'=> 1,
+        'precio'=> 30000,
+        'descripcion'=> 'Capacidad: 32 GB
+                        Formato de la tarjeta: MicroSDHC
+                        Resolución máxima de vídeo: Full HD
+                        Velocidad de lectura: 80 MB/s
+                        Incluye adaptador SD: Sí',        
+        'categoria'=> 3,
+        'imagenL'=> '18.jpg',
+        'imagenS1'=> '19.jpg',
+        'imagenS2'=> '19.jpg',
+        'imagenS3'=> '19.jpg',
+        'comentarios'=> 1,
+        'activa'=> TRUE
+
+    ),
+    
+    '8'=> array(
+        'id'=> 8,
+        'nombre'=>'Camara compacta',
+        'marca'=> 1,
+        'precio'=> 89000,
+        'descripcion'=> 'Tipo de cámara: DSLR
+                         Tamaño de la pantalla: 3 "
+                         Resolución: 16.3 Mpx',        
+        'categoria'=> 2,
+        'imagenL'=> '20.jpg',
+        'imagenS1'=> '21.jpg',
+        'imagenS2'=> '21.jpg',
+        'imagenS3'=> '21.jpg',
+        'comentarios'=> 1,
+        'activa'=> TRUE
+
+    ),
+
+    '9'=> array(
+        'id'=> 9,
+        'nombre'=>'Tarjeta de memoria',
+        'marca'=> 2,
+        'precio'=> 89000,
+        'descripcion'=> 'Capacidad: 2 GB
+                         Formato de la tarjeta: xD-Picture
+                         Incluye adaptador SD: No',        
+        'categoria'=> 3,
+        'imagenL'=> '22.jpg',
+        'imagenS1'=> '22.jpg',
+        'imagenS2'=> '22.jpg',
+        'imagenS3'=> '22.jpg',
+        'comentarios'=> 1,
+        'activa'=> TRUE
+
+    ),
+
+    '10'=> array(
+        'id'=> 10,
+        'nombre'=>'Tarjeta de memoria',
+        'marca'=> 1,
+        'precio'=> 89000,
+        'descripcion'=> 'Capacidad: 64GB
+                         Formato de la tarjeta: xD-Picture
+                         Incluye adaptador SD: No
+                         Nikon D50 D40 D40X D3300 Tarjeta de memoria para camara digital',        
+        'categoria'=> 3,
+        'imagenL'=> '23.jpg',
+        'imagenS1'=> '23.jpg',
+        'imagenS2'=> '23.jpg',
+        'imagenS3'=> '23.jpg',
+        'comentarios'=> 1,
+        'activa'=> TRUE
+
+    ),
+    
+    
 );
 
 echo json_encode($productos);
